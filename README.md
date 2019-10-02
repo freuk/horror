1d7ae087b0889168e5f669f6ddf526e34987e78c
Valentin Reis <valentin.reis@gmail.com>
2019-10-02 16:27:16 -0500

    default file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# horror
+For private packages:
+```
+sudo socat UNIX-LISTEN:/tmp/hax,fork,mode=0070,group=nixbld UNIX-CLIENT:$SSH_AUTH_SOCK
+nix-build -A <packagename> -I ssh-auth-sock=/tmp/hax
+```
---
+++ default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import (builtins.fetchTarball
+  "https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz")
+  { } }:
+
+let
+
+  callPackage = pkgs.lib.callPackageWith pkgs;
+
+in rec {
+
+  vim = callPackage ./vim { };
+
+}
