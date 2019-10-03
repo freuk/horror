9333c5d4bcaf5fb7d70e6e3122df16319a2d7feb
Valentin Reis <vreis@anl.gov>
2019-10-03 08:14:23 -0500

    fixes

--- tig/default.nix
+++ tig/default.nix
@@ -2,7 +2,7 @@
 symlinkJoin {
   name = "tig";
   paths = [
-    (writeScriptBin "tig" "XDG_CONFIG_HOME={builtins.toPath .} tig").out
-    (writeScriptBin "t" "XDG_CONFIG_HOME={builtins.toPath .} tig status").out
+    (writeScriptBin "tig" "XDG_CONFIG_HOME=${builtins.toPath ./.} tig").out
+    (writeScriptBin "t" "XDG_CONFIG_HOME=${builtins.toPath ./.} tig status").out
   ];
 }
