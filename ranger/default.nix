68e18cf9abe3a72e84bdac6ade7f21cc8002223d
Valentin Reis <valentin.reis@gmail.com>
2019-10-10 11:50:16 -0500

    typo fix

--- ranger/default.nix
+++ ranger/default.nix
@@ -1,2 +1,2 @@
 { ranger, writeShellScriptBin }:
-writeShellScriptBin "rangersetup" "mpkdir -p ~/.config/ranger && cp -r ${./config}/* ~/.config/ranger/"
+writeShellScriptBin "rangersetup" "mkdir -p ~/.config/ranger && cp -r ${./config}/* ~/.config/ranger/"
